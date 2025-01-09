22a1d59046bbf8bd55d935b9fdf15884a43d97ac
AbdulAzizFirmansya <abdul.aziz.f01@gmail.com>
2025-01-09 10:01:44 +0700

    Update train.py

--- train.py
+++ train.py
@@ -6,3 +6,6 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
 model.info()
 results = model.train(data='your_path', epochs = 20, imgsz=320)
+# data: your path store the dataset
+#epochs : iteration of your own learn
+# imgsz = image size
