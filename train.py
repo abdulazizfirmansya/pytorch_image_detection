0931da1066c28e8bcaf8be77361956ae36227d4c
AbdulAzizFirmansya <abdul.aziz.f01@gmail.com>
2025-01-09 09:59:24 +0700

    Train.py
    custom train from your dataset

--- train.py
+++ train.py
@@ -1,7 +1,4 @@
 from ultralytics import YOLO
-
 model = YOLO("yolo11n.pt")
-
 model.info()
-
-results = model.train(data='D:\Branch_counter\data\custom_data2.yaml', epochs = 20, imgsz=320)
+results = model.train(data='your_path', epochs = 20, imgsz=320)
