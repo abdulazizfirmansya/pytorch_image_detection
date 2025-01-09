0bfb3c4e07c17a3478733680626b8c4aff8a143b
AbdulAzizFirmansya <abdul.aziz.f01@gmail.com>
2025-01-09 10:17:11 +0700

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ this program how to train object detection using your own custom datasets
 from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
 model.info()
-results = model.train(data='your_path', epochs = 20, imgsz=320)
+results = model.train(data='your_path.yaml', epochs = 20, imgsz=320)
 # data: your path store the dataset
 # epochs : iteration of your own learn
 # imgsz = image size
